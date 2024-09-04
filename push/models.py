402d5fbda062000635dc65fb51dd8d9914104e11
so0nim <308go@naver.com>
2024-09-04 13:20:52 +0900

    feat: push model 작성

--- push/models.py
+++ push/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from sqlalchemy import Boolean, Column, DateTime, Integer
+from sqlalchemy.orm import DeclarativeBase
 
-# Create your models here.
+from cores.models import UtilModel
+
+class PushBase(DeclarativeBase):
+    def __repr__(self):
+        return f"<{self.__tablename__} {self.__dict__}>"
+    
+class Push(PushBase, UtilModel):
+    __tablename__ = "PUSH"
+
+    user_no = Column(Integer, primary_key=True)
+    push_app_ok = Column(Boolean, nullable=False)
+    push_book_ok = Column(Boolean, nullable=False, default=False)
+    push_time = Column(DateTime(timezone=True))
+    
+    
