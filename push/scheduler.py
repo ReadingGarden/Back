e0d6b49337be43bab7f0a67c5e12d99a289423e8
so0nim <308go@naver.com>
2025-01-21 15:36:27 +0900

    feat: 스케쥴러 중복 방지

--- push/scheduler.py
+++ push/scheduler.py
@@ -12,7 +12,10 @@ logger = logging.getLogger("django.server")
 
 # APScheduler를 시작하는 함수
 def start_scheduler():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(job_defaults={
+    'max_instances': 3,  # 모든 작업의 최대 실행 인스턴스 수
+    'coalesce': True     # 이전 작업이 끝나지 않았으면 건너뛰지 않고 대기
+})
 
     # 매 분 정각(초가 0일 때) send_book_push 함수를 실행
     scheduler.add_job(push_service.send_book_push, CronTrigger(second="0"))
@@ -26,5 +29,5 @@ def start_scheduler():
 
     scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
 
-    # 스케줄러 시작
-    scheduler.start()
+    if not scheduler.running:  # 스케줄러가 실행 중인지 확인
+        scheduler.start()
